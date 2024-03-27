8445555a9b3c2b6b5d8b60498dc139a0bffd5ffb
apurbat59 <115810295+apurbat59@users.noreply.github.com>
2024-03-27 14:31:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Software Developer", "Web Developer",],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 500,
+    loop: true
+});
